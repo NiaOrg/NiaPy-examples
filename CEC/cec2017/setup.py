d02bf99250fa70c7340f9005479ae7f60e6fc324
Klemen Berkovic <roxor1992@gmail.com>
2019-11-23 21:38:35 +0100

    Small fixes

--- CEC/cec2017/setup.py
+++ CEC/cec2017/setup.py
@@ -6,12 +6,11 @@ import numpy
 extensions = [
     Extension('cec2017', ['cec2017.pyx', 'cec17_test_func.cpp'],
               include_dirs=[numpy.get_include()],
-              extra_compile_args=['-std=c++17'],
+              extra_compile_args=['-std=c++17', '-w', '-O3', '-march=native'],
               language='c++'
               ),
 ]
 
 setup(
     ext_modules=cythonize(extensions),
-    extra_compile_args=['-w', '-O3', '-march=native'],
 )
