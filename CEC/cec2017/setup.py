fec3d79ca6bfac37b872e9e5f6cf121f7257b4d1
klemen@docker <klemen.berkovic@student.um.si>
2019-11-24 11:47:13 +0000

    Added some fixes

--- CEC/cec2017/setup.py
+++ CEC/cec2017/setup.py
@@ -1,17 +1,19 @@
 import os
 
 from distutils.core import setup
-from Cython.Build import cythonize
 from distutils.extension import Extension
+
+from Cython.Build import cythonize
+
 import numpy as np
 
-extra_compile_args=['-std=c++11', '-w', '-O3', '-march=native']
-if os.getenv('DEBUG', None) is not None: extra_compile_args=['-std=c++11', '-g3', '-O0']
+compile_args=['-std=c++11', '-w', '-O3', '-march=native']
+if os.getenv('DEBUG', None) is not None: compile_args=['-std=c++11', '-g3', '-O0']
 
 extensions = [
     Extension('cec2017', ['cec2017.pyx', 'cec17_test_func.cpp'],
+              extra_compile_args=compile_args,
               include_dirs=[np.get_include()],
-              extra_compile_args=extra_compile_args,
               language='c++'
               ),
 ]
